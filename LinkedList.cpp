47e87ea9c5ac59fbec3edb3a7c801b4fd8248416
rishabhnehra <rishabhnehrapersonal@gmail.com>
2017-04-29 22:28:07 +0530

    Removed insertAfter()

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -35,7 +35,15 @@ void Insert(int data, int position){
 
 void insertAfter(struct node* prev_node, int new_data)//Still needs working
 {
-
+    if (prev_node == NULL)
+    {
+      printf("the given previous node cannot be NULL");
+      return;
+    }
+    struct node* new_node =(struct node*) malloc(sizeof(struct node));
+    new_node->data  = new_data;
+    new_node->next = prev_node->next;
+    prev_node->next = new_node;
 }
 
 
