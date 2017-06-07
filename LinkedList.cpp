38dcd9258d539fe0c852e848cd502fe4d24f74bc
rishabhnehra <rishabhnehrapersonal@gmail.com>
2017-06-08 04:30:44 +0530

    Implemented LinkedList using Classes

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,56 +1,90 @@
 #include<iostream>
 using namespace std;
 
-struct Node {
-	int data;
-	Node *next;
+class Node {
+	public:
+		int value;
+		Node *next;
 };
 
-Node *head = NULL;
-
-void printList(struct Node *n){
-	while(n != NULL){
-		cout <<  n->data << " - ";
-		n = n->next;
-	}
-	cout << endl;
-}
-
-void Insert(int data, int position){
-	if(position == 0){
-		Node *head = new Node();
-		head->data = data;
-		head->next = NULL;
-	}
-
-	Node *temp = head;
-
-	for(int i = 0; i < position; i++){
-		temp = temp->next;
-	}
-
-	temp->data = data;
-	temp->next = NULL;
-}
-
-void insertAfter(struct node* prev_node, int new_data)//Still needs working
+class LinkedList
 {
+	public:
+	Node *head;
 
-}
+	LinkedList(){
+		head = NULL;
+	}
 
+	void append(int value){
+		if(head == NULL){
+			Node *newNode = new Node();
+			newNode->value = value;
+			newNode->next = NULL;
+			head = newNode;
+		}
+		else{
+			Node *current = head;
+			while(current->next != NULL){
+				current = current->next;
+			}
+			Node *newNode = new Node();
+			newNode->value = value;
+			newNode->next = NULL;
+			current->next = newNode;
+		}
+	}
 
-void InsertHead(int data){
-	Node *temp = new Node();
-	temp->data = data;
-	temp->next = head;
-	head = temp;
-}
+	void printList(){
+		Node *current = head;
+		while(current != NULL){
+			cout << current->value << "->";
+			current = current->next;
+		}
+		cout << "NULL" << endl;
+	}
+
+	void printHead(){
+		cout << head->value << endl;
+	}
+
+	void deleteNode(int value){
+		if(head->value == value){
+			head = head->next;
+		}
+		else{
+			Node *current = head;
+			Node *previous;
+			while(current->value != value){
+				previous = current;
+				current = current->next;
+			}
+			previous->next = current->next;
+		}
+	}
+
+	int getPosition(int value){
+		Node *current = head;
+		int counter = 1;
+		while(current->value != value){
+			current = current->next;
+			counter++;
+		}
+		return counter;
+	}
+};
 
 int main(){
-	int n, data;
-	cin >> data;
-	cin >> n;
-	Insert(data, n);
-
+	LinkedList ll;
+	ll.append(1);
+	ll.append(2);
+	ll.append(3);
+	ll.append(4);
+	ll.append(5);
+	ll.printList();
+	ll.printHead();
+	ll.deleteNode(3);
+	ll.printList();	
+	cout << ll.getPosition(5);
 	return 0;
 }
